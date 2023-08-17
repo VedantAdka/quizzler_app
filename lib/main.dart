c107dc2c1baf47ec72f8307828b49b7145de3e58
VedantAdka <vedantadka@gmail.com>
2023-08-17 20:57:04 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quizbrain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+  void checkAnswer(bool userPickedAnswer){
+    bool correctanswer=quizBrain.getCorrectAnswer();
+    setState(() {
+      if(quizBrain.isFinished() == true){
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+        quizBrain.reset();
+        scorekeeper = [];
+      }
+      else {
+        if (userPickedAnswer == correctanswer) {
+          scorekeeper.add(Icon(Icons.check, color: Colors.green,));
+        }
+        else {
+          scorekeeper.add(Icon(Icons.close, color: Colors.red,));
+        }
+
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  // List<String> questions=[
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+  //
+  // List<bool> answers=[
+  //   false,
+  //   true,
+  //   true
+  // ];
+
+  // Question q1=Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
+  // Question q2=Question(q: 'Approximately one quarter of human bones are in the feet.', a: true);
+  // Question q3=Question(q: 'A slug\'s blood is green.', a: true);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Container(
+              //textColor: Colors.white,
+              color: Colors.green,
+              child:  TextButton(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(true);
+                },
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Container(
+              color: Colors.red,
+              child: TextButton(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(false);
+                },
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: scorekeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
